6901303d1ffb2c7e02be905ed7f66cce6e57e366
LinhVuHoang <redtonlinh@gmail.com>
2023-10-10 17:36:08 +0700

    update

--- models/TKB_DS_SinhVien.model.js
+++ models/TKB_DS_SinhVien.model.js
@@ -5,7 +5,7 @@ module.exports = function(){
     this.getAll = async function(MaLopHocPhan,result){
         console.log(MaLopHocPhan)
         var pool = await conn
-        var sqlString = "select MaSinhVien,HoDem,Ten,TenLopHoc,NgaySinh2,MaLopHocPhan,TenMonHoc from View_TKB_LichHocSinhVien where MaLopHocPhan =@MaLopHocPhan GROUP by HoDem,Ten,MaSinhVien,TenLopHoc,NgaySinh2,TenMonHoc,MaLopHocPhan order by Ten"
+        var sqlString = "select a.MaSinhVien,a.MaLopHocPhan,a.HoDem,a.Ten,a.NgaySinh2,a.IDLopHocDanhNghia,a.TenMonHoc,b.TenLop from View_TKB_LichHocSinhVien as a INNER JOIN DM_LopHoc as b on a.IDLopHocDanhNghia=b.Id  where MaLopHocPhan =@MaLopHocPhan GROUP by HoDem,a.MaLopHocPhan,Ten,MaSinhVien,NgaySinh2,TenMonHoc,IDLopHocDanhNghia,b.TenLop order by Ten"
         return await pool.request()
         .input('MaLopHocPhan',sql.NVarChar,MaLopHocPhan)
         .query(sqlString,function(err,data){
