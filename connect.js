7a9872063f3280b4d4f7753e8f164982083e377b
LinhVuHoang <redtonlinh@gmail.com>
2023-10-12 17:33:00 +0700

    update export excel

--- connect.js
+++ connect.js
@@ -1,5 +1,5 @@
-var sql = require('mssql/msnodesqlv8');// for window
-//var sql = require('mssql');
+//var sql = require('mssql/msnodesqlv8');// for window
+var sql = require('mssql');
 //các thông tin kết nối csdl
 var config={
     user:"sa",
@@ -10,9 +10,9 @@ var config={
         trustedConnection: true,
         enableArithAbort: true, 
         trustServerCertificate: true,
-    instanceName: "MSSQLSERVER01"
+//    instanceName: "MSSQLSERVER01"
       },
-    driver: "msnodesqlv8",// for window
+//    driver: "msnodesqlv8",// for window
 };
 const conn = new sql.ConnectionPool(config).connect().then(pool=>{
     console.log("connect successful")
--- models/TKB_DS_SinhVien.model.js
+++ models/TKB_DS_SinhVien.model.js
@@ -6,7 +6,7 @@ module.exports = function(){
         console.log(MaLopHocPhan)
         console.log(MaLopHoc)
         var pool = await conn
-        var sqlString = "SELECT a.IDSinhVien,a.MaLopHoc, a.IDLopHocPhan, a.MaSinhVien, a.MaLopHocPhan, a.HoDem, a.Ten, c.Email, c.SoDienThoai, a.NgaySinh2, a.IDLopHocDanhNghia, a.TenMonHoc, b.TenLop,d.SiSo FROM View_TKB_LichHocSinhVien as a INNER JOIN DM_LopHoc as b on a.IDLopHocDanhNghia = b.Id INNER JOIN DT_SinhVien as c on a.MaSinhVien = c.MaSinhVien INNER JOIN View_SiSo as d on a.IDLopHocPhan = d.IDLopHocPhan WHERE a.MaLopHocPhan = @MaLopHocPhan AND MaLopHoc = @MaLopHoc GROUP by a.HoDem, a.MaLopHocPhan,a.MaLopHoc, a.Ten, a.MaSinhVien, a.NgaySinh2, a.TenMonHoc, IDLopHocDanhNghia, b.TenLop, c.Email, c.SoDienThoai, a.IDSinhVien, a.IDLopHocPhan,d.SiSo ORDER BY Ten;"
+        var sqlString = "SELECT a.IDSinhVien,a.MaLopHoc,a.IDDot,e.TenDot, a.IDLopHocPhan, a.MaSinhVien, a.MaLopHocPhan, a.HoDem, a.Ten, c.Email, c.SoDienThoai, a.NgaySinh2, a.IDLopHocDanhNghia, a.TenMonHoc, b.TenLop,d.SiSo FROM View_TKB_LichHocSinhVien as a INNER JOIN DM_LopHoc as b on a.IDLopHocDanhNghia = b.Id INNER JOIN DT_SinhVien as c on a.MaSinhVien = c.MaSinhVien INNER JOIN View_SiSo as d on a.IDLopHocPhan = d.IDLopHocPhan inner join DM_Dot as e on e.Id = a.IDDot WHERE a.MaLopHocPhan = @MaLopHocPhan AND MaLopHoc = @MaLopHoc GROUP by a.HoDem, a.MaLopHocPhan,a.MaLopHoc, a.Ten, a.MaSinhVien, a.NgaySinh2, a.TenMonHoc, IDLopHocDanhNghia,a.IDDot, b.TenLop, c.Email, c.SoDienThoai, a.IDSinhVien, a.IDLopHocPhan,d.SiSo,e.TenDot ORDER BY Ten;"
         return await pool.request()
         .input('MaLopHocPhan',sql.NVarChar,MaLopHocPhan)
         .input('MaLopHoc',sql.NVarChar,MaLopHoc)
