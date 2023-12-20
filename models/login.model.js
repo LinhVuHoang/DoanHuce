1f6052a19ceade4cdc6d5068f86b0cb60053713b
LinhVuHoang <redtonlinh@gmail.com>
2023-12-21 02:19:21 +0700

    update login

--- models/login.model.js
+++ models/login.model.js
@@ -1,11 +1,14 @@
 const e = require('express');
 const { response, request } = require('express');
+const crypto = require('crypto');
 //const { Request } = require('mssql');
 const {conn,sql} = require('../connect');
 const debug = console.log.bind(console);
 module.exports = function(){
     this.getdata = async function(newData){ 
-        var sqlString = `Select TOP 1 TaiKhoan,MatKhau,IsSuDung,TieuDe from DM_TaiKhoanEmail where TaiKhoan='${newData.Username}' and MatKhau = '${newData.Password}'`;
+        const hashedPassword = crypto.createHash('md5').update(newData.Password).digest('hex');
+        console.log(hashedPassword)
+        var sqlString = `Select TOP 1 * from DM_NhanSuHeThongQLTD where Email='${newData.Username}' and Password = '${hashedPassword}'`;
         console.log(sqlString)
        var pool =await conn;
     const result = (await pool.request().query(sqlString)).recordset[0]
